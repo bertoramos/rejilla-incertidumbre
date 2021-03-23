a4b02e3a9825943117da38f5d5fc48cc5ef17745
darkhorrow <benearosemidan@gmail.com>
2021-03-23 18:22:33 +0000

    Visited neighbours discarded, but there are still issues with the path generated

--- pathfinding/get_path.m
+++ pathfinding/get_path.m
@@ -1,19 +1,28 @@
 function [path] = get_path(map, origin, target)
-    path = [];
-    visited = [];
+    path = [origin];
+    visited = [origin];
     queue = [origin];
     while(~isempty(queue))
         if(isequal(queue(1), target))
+            path(end+1, :) = queue(1);
             break;
         end
         neighbours = nearest_neighbours(map, queue(1, :));
-        path(end+1, :) = queue(1, :);
-        visited(end+1, :) = queue(1, :);
+       
+        for i = size(neighbours, 1)
+            row = neighbours(i, :);
+            
+            for j = size(visited, 1)
+                if row(1) ~= visited(j, 1) && row(2) ~= visited(j, 2)
+                    visited(end+1, :) = row;
+                    path(end+1, :) = row;                  
+                    queue = cat(1, queue, row);
+                end
+            end
+        end
+        
         queue(1, :) = [];
         
-        % Discard already visited neigbours
-        
-        queue = cat(1, queue, neighbours);
     end
 end
 
--- pathfinding/pathfinding.m
+++ pathfinding/pathfinding.m
@@ -1,5 +1,22 @@
 clf;
 
 addpath('../grid_generation');
-main % Call to grid creation file
-get_path(altura, [31 1], [1 31])
+main; % Call to grid creation file
+path = get_path(altura, [13 18], [28 15]);
+
+
+hold on;
+figure;
+
+altura = rejilla;
+altura(altura > 0) = 1;
+
+[x,y] =meshgrid(1:ncell+1, 1:ncell+1);
+s = surf(x,y, altura);
+
+zticks([0 1]);
+
+for i=1:(size(path, 1)-1)
+  pl = line([path(i,1) path(i+1,1)], [path(i,2) path(i+1,2)], [1 1]);
+  pl.Color = 'red';
+end
